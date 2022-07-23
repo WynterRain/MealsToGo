2bfbf278b1628a72dc3fe4da6286037d9c48aaec
SnowiWatermelon <heonaliu@gmail.com>
2022-07-23 16:16:09 -0400

    commit

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { SvgXml } from "react-native-svg";
-
+import { View } from "react-native";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
+import { Favourite } from "../../../components/favourites/favourite.component";
 
 import {
   RestaurantCard,
@@ -35,7 +36,12 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
   return (
     <RestaurantCard elevation={5}>
+      <Favourite />
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <View>
+        <Favourite />
+        <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      </View>
       <Info>
         <Text variant="label">{name}</Text>
         <Section>
